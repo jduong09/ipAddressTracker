e36953ef03af61d89ab7b12679fcceadc6dd7271
Justin Duong <jduong@umich.edu>
2022-09-29 21:54:58 -0400

    Added xml request in order to gather information from geo api and show location on leaflet api. Displayed information of ip given.

--- js/geo.js
+++ js/geo.js
@@ -1,27 +1,70 @@
+const checkInputValidity = (ipAddress) => {
+  const ipSplit = ipAddress.split('.');
+  return ipSplit.every(octet => {
+    const toNum = parseInt(octet);
+    return toNum > 0 && toNum < 250;
+  });
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-  var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'pk.eyJ1IjoiYmxhY2ttb3JlMCIsImEiOiJjaXlub251ZjIwMDJmMnBxems2bmpiYXA2In0.2Hxl5QoDhIY6OR4p3GsU2w'
-}).addTo(map);
   const inputSearch = document.getElementById('input-ip-search');
   const inputSubmit = document.getElementById('input-submit');
+  const listSearchResults = document.querySelector('.search-results > ul');
+
+  const map = L.map('map').setView([34.04915, -118.09462], 16);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
   inputSubmit.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(inputSearch.value);
 
+    if (!checkInputValidity(inputSearch.value)) {
+      console.log('not valid IP Address');
+      return false;
+    }
     const httpRequest = new XMLHttpRequest();
 
-    httpRequest.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        console.log(this.response);
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState === 4) {
+        const response = JSON.parse(httpRequest.response);
+
+        listSearchResults.children[0].children[1].innerHTML = response.ip;
+        listSearchResults.children[1].children[1].innerHTML = `${response.location.city}, ${response.location.region} ${response.location.postalCode}`;
+        listSearchResults.children[2].children[1].innerHTML = `UTC ${response.location.timezone}`;
+        listSearchResults.children[3].children[1].innerHTML = response.isp;
+
+        map.setView([response.location.lat, response.location.lng], 16);
+        L.marker([response.location.lat, response.location.lng]).addTo(map);
       }
     }
 
-    httpRequest.open('GET', 'https://geo.ipify.org/api/v2/country,city?apiKey=at_4DsB8o4wkTQIM6cPVAqz9AKffiVrb&ipAddress=8.8.8.8', true);
+    httpRequest.open('GET', `https://geo.ipify.org/api/v2/country,city?apiKey=at_4DsB8o4wkTQIM6cPVAqz9AKffiVrb&ipAddress=${inputSearch.value}`, true);
     httpRequest.send();
   });
 });
+
+/*
+{
+  "ip":"192.212.174.101",
+  "location": {
+    "country":"US",
+    "region":"California",
+    "city":"South San Gabriel",
+    "lat":34.04915,
+    "lng":-118.09462,
+    "postalCode":"",
+    "timezone":"-07:00",
+    "geonameId":5397771
+  },
+  "as": {
+    "asn":7127,
+    "name":"SCE",
+    "route":"192.212.0.0\/15",
+    "domain":"",
+    "type":""
+  },
+  "isp":"Southern California Edison"
+}
+*/
